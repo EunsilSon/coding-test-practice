e783d6b401c5cf5d1cc7d6391509a57e3358519e
EunsilSon <eunsil1023@gmail.com>
2023-09-04 01:34:24 +0900

    style : [프로그래머스] 추억 점수 주석 제거

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
-        // name, yearning 를 hashmap에 key,value로 저장
+        
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < name.length; i++) {
             map.put(name[i], yearning[i]);
-            //answer.add(0);
         }
 
-        // 외부: photo 길이
-        // 내부: photo[i] 길이
-        // photo[i]를 key 값으로 하고, value를 answer[i]에 누적합
+        int[] answer = new int[photo.length];
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
                 if (map.containsKey(photo[i][j])) {
-                    //answer.set(i, answer.get(i) + map.get(photo[i][j]));
                     answer[i] += map.get(photo[i][j]);
                 }
             }
         }
-
 
         return answer;
     }
