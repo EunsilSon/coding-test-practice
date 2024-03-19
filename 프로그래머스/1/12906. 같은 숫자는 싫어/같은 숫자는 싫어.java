13faad11f7626c7a8c4e6b75999a6826307eb09b
Son Eunsil <eunsil1023@gmail.com>
2024-03-19 23:14:04 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.57 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,6 +3,7 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public int[] solution(int []arr) {
+        /*
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < arr.length-1; i++) {
@@ -18,6 +19,24 @@ public class Solution {
             answer[i++] = num;
         }
 
+        return answer;
+        */
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (!stack.peek().equals(arr[i])) {
+                stack.push(arr[i]);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        int i = stack.size();
+        while (!stack.isEmpty()) {
+            answer[--i] = stack.pop();
+        }
+        
         return answer;
     }
 }
