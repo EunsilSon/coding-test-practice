822b080befad4b53cc2c29e19d8c64c9cf64b863
Son Eunsil <son0010234@gmail.com>
2024-04-05 22:32:07 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,15 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static boolean[] visited;
     static int count = 0;
 
-    /**
-     * DFS (깊이 우선 탐색)
-     * @param network
-     * @param x
-     * @return
-     */
     public static int dfs(List<ArrayList<Integer>> network, int x) {
         visited[x] = true;
 
@@ -22,35 +19,10 @@ public class Main {
         return count;
     }
 
-    /**
-     * BFS (너비 우선 탐색)
-     * @param network
-     * @param start
-     * @return
-     */
-    public static int bfs(List<ArrayList<Integer>> network, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        visited[start] = true;
-        q.offer(start);
-
-        while (!q.isEmpty()) {
-            int x = q.poll();
-
-            for (int i = 0; i < network.get(x).size(); i++) {
-                if (!visited[network.get(x).get(i)]) {
-                    visited[network.get(x).get(i)] = true;
-                    q.offer(network.get(x).get(i));
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int computers = sc.nextInt(); // 컴퓨터의 수
-        int connected = sc.nextInt(); // 직접 연결된 컴퓨터 쌍의 수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computers = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+        int connected = Integer.parseInt(br.readLine()); // 직접 연결된 컴퓨터 쌍의 수
         visited = new boolean[computers+1]; // 방문 체크
 
         List<ArrayList<Integer>> network = new ArrayList<>();
@@ -59,11 +31,12 @@ public class Main {
         }
 
         for (int i = 0; i < connected; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             network.get(x).add(y);
             network.get(y).add(x);
         }
-        System.out.println(bfs(network,1));
+        System.out.println(dfs(network,1));
     }
 }
