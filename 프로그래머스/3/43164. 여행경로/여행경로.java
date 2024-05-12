ce6c2b6e04f9dfa85c15f486737d9ddbc3ac3809
손은실 <son0010234@gmail.com>
2024-05-12 18:48:04 +0900

    [level 3] Title: 여행경로, Time: 10.55 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    static List<String> list = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets) {
+        
+        visited = new boolean[tickets.length];
+        
+        dfs(tickets, "ICN", "ICN", 0);
+        Collections.sort(list);
+        return list.get(0).split(" ");
+        
+    }
+    
+    public static void dfs(String[][] tickets, String start, String route, int depth) {
+        if (depth == tickets.length) {
+            list.add(route);
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i][0].equals(start) && !visited[i]) {
+                visited[i] = true;
+                dfs(tickets, tickets[i][1], route + " " + tickets[i][1], depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
