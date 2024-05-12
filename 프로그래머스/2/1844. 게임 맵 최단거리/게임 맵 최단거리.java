703f578f5efec2e326642e8a7b4ce5a634e7598c
손은실 <son0010234@gmail.com>
2024-05-13 01:34:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.82 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,57 +1,51 @@
 import java.util.*;
 
-class Solution {
-    static class Node {
-        int x, y, cnt;
+class Node {
+    int x, y, d;
 
-        Node(int x, int y, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
+    Node (int x, int y, int d) {
+        this.x = x;
+        this.y = y;
+        this.d = d;
     }
+}
+
+class Solution {
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
     
-    static int bfs(int n, int m, int[][] maps) {
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        
+    public static int bfs(int[][] maps) {
         Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0,0, 1));
+        queue.add(new Node(0, 0, 1));
         maps[0][0] = 0;
 
         while (!queue.isEmpty()) {
             Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
 
             for (int i = 0; i < 4; i++) {
-                // 상, 하, 좌, 우 방향 이동
-                int pX = node.x + dx[i];
-                int pY = node.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                // 상대 팀 진영 체크
-                if (pX == n && pY == m) {
-                    return node.cnt+1;
+                if (nx == maps.length-1 && ny == maps[0].length-1) {
+                    return node.d + 1;
                 }
 
-                // 유효 범위 체크
-                if (pX < 0 || pY < 0 || pX > n || pY > m) {
+                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length) {
                     continue;
                 }
 
-                // 캐릭터 이동
-                if (maps[pX][pY] == 1) {
-                    queue.offer(new Node(pX, pY, node.cnt+1));
-                    maps[pX][pY] = 0; // 방문 체크
+                if (maps[nx][ny] == 1) {
+                    maps[nx][ny] = 0;
+                    queue.add(new Node(nx, ny, node.d + 1));
                 }
-
             }
         }
         return -1;
     }
     
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-
-        return bfs(n-1, m-1, maps);
+        return bfs(maps);
     }
 }
