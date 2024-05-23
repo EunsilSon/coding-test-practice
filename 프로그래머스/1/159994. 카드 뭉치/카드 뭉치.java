53e969cd154069c47c6d5d03daca0fc0b0a83cb9
손은실 <son0010234@gmail.com>
2024-05-23 14:09:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.19 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> q1 = new LinkedList<>();
+        Queue<String> q2 = new LinkedList<>();
+
+        for (String s : cards1) {
+            q1.add(s);
+        }
+
+        for (String s : cards2) {
+            q2.add(s);
+        }
+
+        for (int i = 0; i < goal.length; i++) {
+            if (!q1.isEmpty() && goal[i].equals(q1.peek())) { // cards1에서 사용
+                q1.poll();
+                continue;
+            }
+
+            if (!q2.isEmpty() && goal[i].equals(q2.peek())) { // cards2에서 사용
+                q2.poll();
+                continue;
+            }
+            // 둘 다 맞는 게 없음
+            return "No";
+        }
+        return "Yes";
+    }
+}
