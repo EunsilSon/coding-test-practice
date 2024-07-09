fc0405a24c891dd5c3bac147496235f3480b1536
손은실 <son0010234@gmail.com>
2024-07-09 17:36:42 +0900

    [Silver II] Title: N과 M (11), Time: 496 ms, Memory: 37240 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -2,52 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static int[] arr;
-    static int[] numbers;
-    static LinkedHashSet<String> set = new LinkedHashSet<>();
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st1.nextToken());
-        M = Integer.parseInt(st1.nextToken());
-
-        numbers = new int[N];
-        arr = new int[M];
-        visited = new boolean[N+1];
-
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        Arrays.sort(numbers); // 정렬
-        duplicatePermutation(0);
-
-        for (String s : set) {
-            System.out.println(s);
-        }
-
-    }
-
-    public static void duplicatePermutation(int depth) {
-        if (depth == M) {
-            StringBuilder sb = new StringBuilder();
-            for (int n : arr) {
-                sb.append(n).append(" ");
-            }
-
-            set.add(sb.toString().trim()); // 시간 초과 방지
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            arr[depth] = numbers[i];
-            duplicatePermutation(depth + 1);
-        }
-
-    }
+	
+	static int N, M;
+	static int[] arr, printArr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[N];
+		printArr = new int[M];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		dfs(0);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int depth) {
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(printArr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int before = -1;
+		for(int i=0; i<N; i++) {
+			int now = arr[i];
+			if(before != now) {
+				before = now;
+				printArr[depth] = arr[i];
+				dfs(depth + 1);
+			}
+				
+		}
+	}
 }
