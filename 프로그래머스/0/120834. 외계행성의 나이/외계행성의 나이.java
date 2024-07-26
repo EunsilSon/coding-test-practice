dcc43ddcaa492da1ea0dfe9136e84ee23b1d83e2
손은실 <son0010234@gmail.com>
2024-07-26 14:39:22 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(int age) {
-        char[] alphabets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] alphabets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
         
         String s = Integer.toString(age);
         StringBuilder sb = new StringBuilder();
