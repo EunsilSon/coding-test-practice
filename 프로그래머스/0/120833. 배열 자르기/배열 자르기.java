fccea56de4bc8ec6726602dddb902e5e81fd3dc0
손은실 <son0010234@gmail.com>
2024-07-26 14:31:10 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = num1; i <= num2; i++) {
+            list.add(numbers[i]);
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
