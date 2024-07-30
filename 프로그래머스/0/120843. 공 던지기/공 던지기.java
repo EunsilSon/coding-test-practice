fc0179e7bf1d1d2b02f789888e3e7a09ef36e7dd
손은실 <son0010234@gmail.com>
2024-07-31 00:18:46 +0900

    [level 0] Title: 공 던지기, Time: 0.92 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        
         
         Queue<Integer> queue = new LinkedList<>();
         for (int n : numbers) {
             queue.add(n);
         }
         
-        
-        int count = 0;
-        
+        int count = 1;
         while (count != k) {
-            count++;
-            
-            answer = queue.remove(); // 공 던진 사람
-            queue.add(answer);
             queue.add(queue.remove());
+            queue.add(queue.remove());
+            count++;
         }
-    
         
-        return answer;
+        return queue.remove();
+        
     }
 }
