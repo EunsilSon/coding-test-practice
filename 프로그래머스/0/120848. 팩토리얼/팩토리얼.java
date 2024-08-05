2f28843f6e60488c2a67e37cb600509673dc8807
손은실 <son0010234@gmail.com>
2024-08-06 00:55:38 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,24 +1,14 @@
 class Solution {
     public int solution(int n) {
-        
         int i = 1;
+        int factorial = 1;
         
         while (true) {
-            if (factorial(i) > n) {
-                break;
-            }
+            factorial *= i;
+            if (factorial > n) break;
             i++;
         }
         
         return i-1;
-        
-    }
-    
-    public static int factorial(int n) {
-        if (n <= 1) {
-            return 1;
-        }
-        
-        return factorial(n - 1) * n;
-    }
+    } 
 }
