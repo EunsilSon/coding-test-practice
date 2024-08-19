09ecc038c5dd10e78d3c730cba0fc186074417d2
손은실 <son0010234@gmail.com>
2024-08-20 00:10:17 +0900

    [level 0] Title: 배열 원소의 길이, Time: 3.37 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist)
+            .mapToInt(String::length)
+            .toArray();
+        
+    }
+}
