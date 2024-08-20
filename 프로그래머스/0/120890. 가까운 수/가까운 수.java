72fdb0e66d7476e57ccfe9201629ed559093e31e
손은실 <son0010234@gmail.com>
2024-08-20 15:46:17 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int distance = 100;
+        
+        for (int i : array) {
+            int temp = (i > n) ? (i - n) : (n - i);
+            
+            if (distance > temp) {
+                distance = temp;
+                answer = i;
+                continue;
+            }
+            
+            if (distance == temp) {
+                distance = temp;
+                answer = answer > i ? i : answer;
+            }
+        }
+        
+        return answer;
+    }
+}
