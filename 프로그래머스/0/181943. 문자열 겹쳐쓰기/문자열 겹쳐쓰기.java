e5bd251829e168a73859a5c1181358ab3cccf597
손은실 <son0010234@gmail.com>
2024-09-13 11:56:19 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 1.96 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,19 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < s; i++) {
-            sb.append(my_string.charAt(i));
-        }
-        
-        for (int i = 0; i < overwrite_string.length(); i++) {
-            sb.append(overwrite_string.charAt(i));
-        }
-        
-        for (int i = (s+overwrite_string.length()); i < my_string.length(); i++) {
-            sb.append(my_string.charAt(i));
-        }
-        
-        return sb.toString();
+        String result = "";
+        result += my_string.substring(0, s);
+        result += overwrite_string;
+        result += my_string.substring(overwrite_string.length() + s);
+        return result;
     }
 }
