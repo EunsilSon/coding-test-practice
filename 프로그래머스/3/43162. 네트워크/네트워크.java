81d6210aa9d431db4796be40ac61e0b4079f3ee5
손은실 <son0010234@gmail.com>
2024-09-24 23:46:34 +0900

    [level 3] Title: 네트워크, Time: 2.41 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int network = 0;
+        
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers.length; j++) {
+                if (computers[i][j] == 1) {
+                    dfs(computers, i);
+                    network++;
+                }
+            }
+        }
+        return network;
+    }
+    
+    public static void dfs(int[][] computers, int x) {
+        for (int y = 0; y < computers[x].length; y++) {
+            if (computers[x][y] == 1) {
+                computers[x][y] = 0;
+                dfs(computers, y);
+            }
+        }
+    }
+    
+}
