9b8954047fddac0e9d70ff78b186b6a25e53daad
손은실 <son0010234@gmail.com>
2024-09-25 20:11:03 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,60 +1,61 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int N;
-    static int[][] aptMap;
-    static ArrayList<Integer> aptList;
-    static int aptCount;
-
-    public static boolean dfs(int i, int j) {
-        if (i < 0 || j < 0 || i >= N || j >= N) {
-            return false;
-        }
-
-        if (aptMap[i][j] == 1) {
-            aptMap[i][j] = 0;
-            aptCount++;
-
-            dfs(i-1, j);
-            dfs(i+1, j);
-            dfs(i, j-1);
-            dfs(i, j+1);
-            return true;
-        }
-
-        return false;
-    }
+class Main {
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int apt = 0;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        N = Integer.parseInt(br.readLine());
-        aptMap = new int[N][N];
-        aptList = new ArrayList<>();
-        aptCount = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] graph = new int[n][n];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             String[] line = br.readLine().split("");
-            for (int j = 0; j < N; j++) {
-                aptMap[i][j] = Integer.parseInt(line[j]);
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0;  j < N; j++) {
-                aptCount = 0;
-                if (aptMap[i][j] == 1) {
-                    dfs(i, j);
-                    aptList.add(aptCount);
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                apt = 0;
+
+                if (graph[i][j] == 1) {
+                    dfs(graph, i, j);
+                    list.add(apt);
+                    count++;
                 }
             }
         }
 
-        System.out.println(aptList.size());
-        Collections.sort(aptList);
+        System.out.println(count);
+        Collections.sort(list);
+        for(int num : list) {
+            System.out.println(num);
+        }
+    }
 
-        for (int n : aptList) {
-            System.out.println(n);
+    public static void dfs(int[][] graph, int x, int y) {
+        if (x < 0 || y < 0 || x >= graph.length || y >= graph[x].length) {
+            return;
+        }
+
+        if (graph[x][y] == 1) {
+            graph[x][y] = 0;
+            apt++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                dfs(graph, nx, ny);
+            }
         }
     }
 }
