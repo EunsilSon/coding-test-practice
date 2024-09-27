ea41d08f546c9583b1055560684ce750422d2406
손은실 <son0010234@gmail.com>
2024-09-27 18:52:42 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int answer = 0;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (answer <= arr[i][j]) {
+                    answer = arr[i][j];
+                    x = i+1;
+                    y = j+1;
+                }
+            }
+        }
+        System.out.println(answer);
+        System.out.println(x + " " + y);
+    }
+}
